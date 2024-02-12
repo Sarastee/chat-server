f5882046ebd4ee0a1cf050f01c5dfff5cb6a570d
Ilya Lyakhov <ilja.sarasti@mail.ru>
2024-02-12 20:53:22 +0500

    Stage 1 complete

---
+++ cmd/grpc-server/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	desc "github.com/sarastee/chat-server/pkg/chat_api_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"log"
+	"net"
+)
+
+const (
+	grpcPort = 50052
+)
+
+type server struct {
+	desc.UnimplementedChatAPIV1Server
+}
+
+type Chat struct {
+	ID        int64
+	Usernames []string
+}
+
+var testChat Chat
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+
+	testChat.ID = 0
+	testChat.Usernames = req.Usernames
+
+	log.Printf("Chat has been created: %+v", testChat)
+
+	return &desc.CreateResponse{
+		Id: testChat.ID,
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	if req.Id != testChat.ID {
+		return nil, fmt.Errorf("chat %d not found", req.Id)
+	}
+
+	testChat = Chat{}
+
+	log.Printf("Chat %d was deleted", req.Id)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Message: \"%v\" from: %v at %v", req.Message.Text, req.Message.From, req.Message.Timestamp.AsTime())
+
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatAPIV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
